79b6152998e2e5f30984f076e8bd7c489eea7266
Mohamed Saud <mohammed.s3oud@gmail.com>
2023-11-19 11:05:34 -0800

    README initial

---
+++ README.md
@@ -0,0 +1,57 @@
+# MircroServiceTeam
+
+**How to programmatically REQUEST data:**
+You can request this microservice to run by writing the word “run” into a “request.txt” file.  
+**How to programmatically RECEIVE data:**
+The program then will call a function to make an API request and write the results in a “response.json” file.
+
+An example of the response json file will look like this: 
+{
+    "coord": {
+        "lon": -122.6762,
+        "lat": 45.5234
+    },
+    "weather": [
+        {
+            "id": 501,
+            "main": "Rain",
+            "description": "moderate rain",
+            "icon": "10n"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 280.32,
+        "feels_like": 276.93,
+        "temp_min": 278.81,
+        "temp_max": 282.48,
+        "pressure": 1004,
+        "humidity": 87
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 5.66,
+        "deg": 130
+    },
+    "rain": {
+        "1h": 1.59
+    },
+    "clouds": {
+        "all": 100
+    },
+    "dt": 1700359163,
+    "sys": {
+        "type": 2,
+        "id": 2008548,
+        "country": "US",
+        "sunrise": 1700320433,
+        "sunset": 1700354287
+    },
+    "timezone": -28800,
+    "id": 5746545,
+    "name": "Portland",
+    "cod": 200
+}
+
+**UML sequence diagram**
+![image](UML.png)
---
+++ TeamMicroservice.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import time
+
+def call_api_and_save_to_json(url: str, filename: str):
+    """
+    Calls an API and saves the response to a JSON file.
+ 
+    Parameters:
+    - url: str
+        The URL of the API to call.
+    - filename: str
+        The name of the JSON file to save the response to.
+ 
+    Raises:
+    - requests.exceptions.RequestException:
+        If there is an error while making the API request.
+    - IOError:
+        If there is an error while saving the response to the JSON file.
+    """
+    try:
+        # Make the API request
+        response = requests.get(url).json()
+        json_Obj = json.dumps(response)
+
+        with open(filename, 'w') as file:
+            file.write(json_Obj)
+ 
+        print(f"Response saved to {filename} successfully.")
+ 
+    except requests.exceptions.RequestException as e:
+        print(f"Error while making API request: {e}")
+ 
+    except IOError as e:
+        print(f"Error while saving response to JSON file: {e}")
+ 
+# Example usage:
+with open("APIKey.txt", 'r') as file:
+    APIKey = file.readline().strip()
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+api_url = base_url + "appid=" + APIKey +"&q=" + "PORTLAND"
+json_filename = "response.json"
+
+
+#call_api_and_save_to_json(api_url, json_filename)
+
+while True:
+    time.sleep(1)
+    f = open("Request.txt", 'r+')
+    ReadData = f.readline().strip()
+    if ReadData == "run":    
+        call_api_and_save_to_json(api_url, json_filename)
+        f.seek(0)
+        f.truncate(0)        
+        f.close()
+    f.close()
